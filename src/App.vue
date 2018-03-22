94874048a674d44b77ed3f7c33aa70d14e93ff1c
zebzhong <bin_zhong@163.com>
2018-03-22 23:12:31 +0800

    增加图片

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+<div id="app">
+  <transition name="router-fade">
+    <router-view></router-view>
+  </transition>
+  <div class="border"></div>
+</div>
 </template>
 
 <script>
@@ -9,3 +12,20 @@ export default {
   name: 'App'
 }
 </script>
+<style lang="scss">
+@import './style/reset';
+.router-fade-enter-active, .router-fade-leave-active {
+  transition: opacity .3s;
+}
+.router-fade-enter, .router-fade-leave-active {
+  opacity: 0;
+}
+.border{
+  width: 32px;
+  height: 40px;
+  background-color: orange;
+  border-bottom: 1px solid transparent;
+  margin-bottom: 15px;
+	border-image: svg(1px-border param(--color #333)) 2 2 stretch;
+}
+</style>
